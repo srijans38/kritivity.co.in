3ccab1fa187cf2a059bd695160b300ba1fb5f401
Srijan Sharma <srijans38@gmail.com>
2021-06-23 01:11:29 +0530

    refactor: seperate date function

--- pages/[slug].js
+++ pages/[slug].js
@@ -7,25 +7,7 @@ import BlockContent from '@sanity/block-content-to-react';
 import { getPostDataBySlug, getSlugs } from '../lib/sanity';
 import { getBlurredImage } from '../lib/blurImages';
 import { getImageSerializer } from '../lib/getImageSerializer';
-
-const getDateString = (date) => {
-  const d = new Date(date);
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  return `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
-};
+import { getDateString } from '../lib/getDateString';
 
 export default function Post({ data: { post } }) {
   return (
