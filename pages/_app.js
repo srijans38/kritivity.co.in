ff840a91d868f6ac6ef0a628221f9a074e6e19e4
Srijan Sharma <srijans38@gmail.com>
2021-06-25 05:11:53 +0530

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,24 @@ import '../styles/globals.css';
 import { AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
+
+import * as ga from '../lib/analytics';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <div className="container">
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&family=Josefin+Sans:wght@400;600&family=Raleway:wght@400;700&display=swap"
